e36b3e30c22dbd87ceed28056f861501230e4d03
Aamir Farooq <aamir@farooq.xyz>
2019-12-25 00:05:32 +0500

    anna oop

--- agent.py
+++ agent.py
@@ -181,7 +181,7 @@ def resolveMoveNoPath(board, head_position):
             if board[neighborPos[0]][neighborPos[1]] == GameObject.EMPTY or board[neighborPos[0]][neighborPos[1]] == GameObject.FOOD:
                 path.append(neighborPos)
                 return path
-    print("Goodbye cruel world, i'm stuck :("))
+    print("Goodbye cruel world, i'm stuck :(")
     return path
 
 
@@ -189,7 +189,7 @@ class Agent:
 
     def __init__(self):
         """" Constructor of the Agent, can be used to set up variables """
-        self.path=[]
+        self.path = []
 
     def get_move(self, board, score, turns_alive, turns_to_starve, direction, head_position, body_parts):
         """This function behaves as the 'brain' of the snake. You only need to change the code in this function for
@@ -234,22 +234,22 @@ class Agent:
         """
 
         if len(self.path) <= 1:
-            self.path=astar(head_position, board,
+            self.path = astar(head_position, board,
                               score, body_parts)
 
         # print(
         #     "Current position: {0}\nPath: {1}]\nScore: {2}\n-----".format(head_position, self.path, score))
 
         if self.path:
-            tempMove=resolveMovePath(self.path, direction, head_position)
+            tempMove = resolveMovePath(self.path, direction, head_position)
             del self.path[0]
             return tempMove
         else:
-            self.path=resolveMoveNoPath(board, head_position)
+            self.path = resolveMoveNoPath(board, head_position)
             # time.sleep(1)
             if self.path:
                 if len(self.path) > 1:
-                    tempMove=resolveMovePath(
+                    tempMove = resolveMovePath(
                         self.path, direction, head_position)
                     del self.path[0]
                     return tempMove
